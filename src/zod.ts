08c2a5022ddacf2d195bb3bb422b62fd7c3b8eaa
sahir <sahirchawla2004@gmail.com>
2026-06-10 13:56:38 +0530

    feat: handling nested obj and arr

--- src/zod.ts
+++ src/zod.ts
@@ -1,12 +1,18 @@
-import {z, ZodObject} from "zod"
+import {z, ZodObject, ZodType} from "zod"
 import { SchemaType } from "./types";
 export const ParseSchema = (obj?:SchemaType)=>{
     if(!obj) return undefined
     const schema: Record<string, any> = {};
     for(let prop of obj){
         if(Array.isArray(prop.type)){
-            schema[prop.field] = ParseSchema(prop.type)
-        }else{
+            if(prop.array === true){
+                schema[prop.field] = z.array(ParseSchema(prop.type) as ZodType<any>)
+            }
+            else{
+                schema[prop.field] = ParseSchema(prop.type) as ZodType<any>
+            }
+        }
+        else{
             schema[prop.field] = !prop.required?z[prop.type]().optional():z[prop.type]({required_error:prop.field+" is required"})
         }
     }
